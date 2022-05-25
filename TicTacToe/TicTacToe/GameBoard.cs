b762d1ffa0e03bfade5dc7bac52ff97ff2a1f7ea
dainist <dainis.st@gmail.com>
2022-05-25 21:01:24 +0300

    Maijs 25

---
+++ TicTacToe/TicTacToe/GameBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameBoard
+    {
+        static void Board(string[] args)
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("     |     |      ");
+        }
+       
+
+
+    }
+}
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,3 +1,17 @@
-﻿
-//Tests
+﻿//1.Provide instructions
+//1.1.A greeting
+//1.2.Display player sign, Player 1 is X and Player 2 is O
+//1.3.	 Who’s turn is it?
+//1.4.	 Instruc the user to enter a number between 1 and 9
+//2.	Draw the game board
+//2.1.	 Game will have 3 rows and 3 columns will be numbered 1 through 9
+//3.	As the user places markers on the game update the board then notify which player has a turn
+//3.1.	 After each turn judge if there is a winner
+//3.2.	 If no winner keep playing by going to step 1.
+//3.3.	 If all markers are placed and no winner then it’s a draw stop the game
+//3.4.	 If we have a winner announce who won and stop the game
+
+using TicTacToe;
+
+GameBoard.
 
